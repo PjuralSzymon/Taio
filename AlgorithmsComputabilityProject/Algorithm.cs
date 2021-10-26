3312a1d0f2584ca101ac676da4826efdc23384b6
Mateusz Kubiszewski <63213544+MateuszKubiszewski@users.noreply.github.com>
2021-10-26 23:04:06 +0200

    czyzby dzialalo :OOOOOOOOOO

--- AlgorithmsComputabilityProject/Algorithm.cs
+++ AlgorithmsComputabilityProject/Algorithm.cs
@@ -21,11 +21,21 @@ namespace AlgorithmsComputabilityProject
             int maxCommonEdges = 0;
             foreach (Matrix M in new IsomorphicGenerator(A))
             {
-                M.Print();
+                //M.Print();
                 for (int x = 0; x <= M.VerticesNumber - B.VerticesNumber; x++)
                 {
                     for (int y = 0; y <= M.VerticesNumber - B.VerticesNumber; y++)
                     {
+                        //foreach (Matrix N in new IsomorphicGenerator(B))
+                        //{
+                        //    Matrix subMatrix = M.GetSubMatrix(x, y, N.VerticesNumber);
+                        //    subMatrix = Matrix.FindCommonMatrix(subMatrix, N);
+                        //    if (subMatrix.EdgesNumber > maxCommonEdges)
+                        //    {
+                        //        maxCommonEdges = subMatrix.EdgesNumber;
+                        //        BiggestSubGraph = subMatrix;
+                        //    }
+                        //}
                         Matrix subMatrix = M.GetSubMatrix(x, y, B.VerticesNumber);
                         subMatrix = Matrix.FindCommonMatrix(subMatrix, B);
                         if (subMatrix.EdgesNumber > maxCommonEdges)
